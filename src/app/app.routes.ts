2eee8ca9c2af1c3d7180d23dda645123dc281e95
cristian-siguenza16 <csiguenzavelasquez@gmail.com>
2025-12-08 19:14:19 -0600

    Created routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Person } from './pages/person/person';
+import { Home } from './pages/home/home';
+import { FormPerson } from './pages/form-person/form-person';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component:Home },
+  { path: 'login', component:Login },
+  { path: 'person/create', component:FormPerson },
+  { path: 'person/:id', component:Person },
+  { path: 'person/edit/:id', component:FormPerson },
+  { path: '**', redirectTo: '' },
+];
