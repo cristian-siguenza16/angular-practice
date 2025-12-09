1e5bb7c648ed0f7d30625ec76e7f7e33c6f2bdfc
cristian-siguenza16 <csiguenzavelasquez@gmail.com>
2025-12-08 19:16:40 -0600

    Added environment variables.

---
+++ src/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  // 👈 Añade tus variables aquí
+  clientId: 'spring-boot-api-rest',
+  clientSecret: 'LVIB1qCcvNVqeUXhakBrvVPS7AUXFY4I',
+  apiBaseUrl: 'http://localhost:8080',
+  apiBaseKeycloakUrl: 'http://localhost:9090/realms/spring-boot-dev/protocol/openid-connect'
+};
